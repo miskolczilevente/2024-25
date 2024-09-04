64acaea803d7f65e7b5ea785f668065af81abc9a
Levente Tamás Miskolczi <miskolczi.levente@ckik.hu>
2024-09-04 07:36:24 +0200

    first

---
+++ Szabdan/Asztali/2024.09.04 Ismétlés/2024.09.04 Ismétlés/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2024._09._04_Ismétlés
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Ismétlés
+             * Vezérlő szerkezetek (utasítás, elágazás, ciklus)
+             * Programozási tételek
+             * Metódusok (függvény, eljárás)
+             * Osztályok (class)
+             * Öröklödés + scope
+             * Fájlbeolvasás
+             */
+
+        }
+    }
+}
