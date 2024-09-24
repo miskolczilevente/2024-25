2c633f759752cd2e2395f17e9841ac34f53c6937
Levente Tamás Miskolczi <miskolczi.levente@ckik.hu>
2024-09-24 09:36:07 +0200

    pitypang

---
+++ Szabdan/Asztali/2024.09.24 Pitypang/2024.09.24 Pitypang/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2024._09._24_Pitypang
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+    }
+}
