a93fb7f03fe263c831eba0daad30a2cc79f14b3e
Levente Tamás Miskolczi <miskolczi.levente@ckik.hu>
2024-09-25 08:32:30 +0200

    Pitypang class

---
+++ Szabdan/Asztali/2024.09.25 ClassokGyakorlasa/2024.09.25 ClassokGyakorlasa/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.ComponentModel;
+
+namespace _2024._09._25_ClassokGyakorlasa
+{
+    internal class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            Fajlbeolvasas();
+
+        }
+        static void Fajlbeolvasas()
+        {
+            PitypangBeolv("pitypang.txt");
+        }
+
+        
+    }
+    class Feladatok
+    {
+        public Feladatok() 
+        {
+            
+        }
+
+    }
+
+    class Pitypang
+    {
+
+        public static List<Pitypang> pitypangok = new List<Pitypang>();
+
+        int sorszam, szobaszam, erkezes_napja, tavozas_napja, vendegek_szama;
+        bool reggeli;
+        string nev;
+
+        public Pitypang(int sorszam, int szobaszam, int erkezes_napja, int tavozas_napja, int vendegek_szama, bool reggeli, string nev)
+        {
+            this.sorszam = sorszam;
+            this.szobaszam = szobaszam;
+            this.erkezes_napja = erkezes_napja;
+            this.tavozas_napja = tavozas_napja;
+            this.vendegek_szama = vendegek_szama;
+            this.reggeli = reggeli;
+            this.nev = nev;
+        }
+
+        static void PitypangBeolv(string eleres)
+        {
+            StreamReader sr = new StreamReader(eleres);
+
+            int n = Convert.ToInt32(sr.ReadLine());
+
+            while (!sr.EndOfStream)
+            {
+                string[] list = sr.ReadLine().Split(' ');
+                Pitypang dr = new Pitypang(Convert.ToInt32(list[0]), Convert.ToInt32(list[1]), Convert.ToInt32(list[2]), Convert.ToInt32(list[3]), Convert.ToInt32(list[4]), Convert.ToBoolean(list[5]), list[6]);
+                pitypangok.Add(dr);
+            }
+            sr.Close();
+        }
+    }
+
+}
---
+++ Szabdan/Asztali/2024.09.25 StructVSClass/2024.09.25 StructVSClass/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2024._09._25_StructVSClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AutoStruct astruct = new AutoStruct(1600);
+            AutoClass aclass = new AutoClass(1500);
+
+            Console.WriteLine("struct: " + astruct.hut);
+            Console.WriteLine("class: " + aclass.hut);
+
+
+            ValtoztatStruct(astruct);
+            ValtoztatClass(aclass);
+
+            Console.WriteLine("struct: " + astruct.hut);
+            Console.WriteLine("class: " + aclass.hut);
+
+            ValtoztatStruct(ref astruct);
+
+            Console.WriteLine("struct: " + astruct.hut);
+
+            Console.WriteLine("struct cím átadással: " + astruct.hut);
+
+            Console.ReadLine();
+        }
+
+        static void ValtoztatStruct(AutoStruct astruct)
+        {
+            astruct.hut = 0;
+        }
+
+        static void ValtoztatStruct(ref AutoStruct astruct)
+        {
+            astruct.hut = 0;
+        }
+        static void ValtoztatClass(AutoClass aclass)
+        {
+            aclass.hut = 0;
+        }
+
+    }
+    struct AutoStruct
+    {
+        public int hut; //hengerűrtartalom
+
+        public AutoStruct(int hut)
+        {
+            this.hut = hut;
+        }
+    }
+
+    class AutoClass
+    {
+        public int hut; //hengerűrtartalom
+
+        public AutoClass(int hut)
+        {
+            this.hut = hut;
+        }
+    }
+}
