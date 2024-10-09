7d6949213931b09c0a51564df6e1726dadf33d46
Levente Tamás Miskolczi <miskolczi.levente@ckik.hu>
2024-10-09 12:24:45 +0200

    niga

---
+++ Szabdan/Asztali/2024.10.09 Oroklodes/ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Kutya k = new Kutya();
+
+            //k.ugat();
+
+            Puli p = new Puli();
+            p.Terel();
+
+            p.ugat();
+
+            Kutya k = p;
+            k.ugat();
+            //k.Terel(); //polimorfizmus miatt nem megoldható
+
+            Console.ReadLine();
+        }
+    }
+    class Kutya
+    {
+        public void ugat()
+        {
+            Console.WriteLine("Vau-Vau");
+        }
+    }
+
+    class Puli : Kutya
+    {
+        public void Terel()
+        {
+            Console.WriteLine("Gyerünk, indulás, lusta banda!");
+        }
+    }
+}
---
+++ Szabdan/Asztali/2024.10.09 Oroklodes2/2024.10.09 Oroklodes2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2024._10._09_Oroklodes2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ososztaly os = new Ososztaly(123);
+            //os.x = 123;
+            //Console.WriteLine(os.ToString());
+
+            Utodosztaly utod = new Utodosztaly(543,654);
+            //utod.b = 543;
+            //Console.WriteLine(utod.ToString());
+
+            Console.ReadLine();
+
+        }
+    }
+
+    class Ososztaly
+    {
+        public int x;
+        public void Tobbszoroz(int a) 
+        {
+            x *= a;
+        }
+
+        public Ososztaly(int x)
+        {
+            this.x = x;
+        }
+
+        public override string ToString()
+        {
+            return "x= " + x;
+        }
+    }
+
+    class Utodosztaly : Ososztaly 
+    {
+        //Mezök
+        public int b;
+
+        //Konstruktor
+        public Utodosztaly(int b, int x) : base(x)
+        {
+
+        }
+        public void TobbszorozB(int k) 
+        {
+            b *= k;
+        }
+
+        public override string ToString()
+        {
+            return "b= " + b + "\tx= "+x;
+        }
+    }
+
+}
