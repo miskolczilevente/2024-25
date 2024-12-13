a81db5137990c62006b234bf4a2fe01f57a7577f
Levente Tamás Miskolczi <miskolczi.levente@ckik.hu>
2024-12-13 12:24:45 +0100

    hell nawh

---
+++ Fnorbi/Web/3-tombfuggvenyek/3-emberek.js
@@ -0,0 +1,16 @@
+const people = [
+    { name: "Dani", age: 47 },
+    { name: "Kata", age: 31 },
+    { name: "Laci", age: 18 },
+    { name: "Béla", age: 62 },
+    { name: "Anikó", age: 24 },
+    { name: "Gábor", age: 39 },
+    { name: "Eszter", age: 28 },
+    { name: "János", age: 52 },
+    { name: "József", age: 24 },
+    { name: "Tibor", age: 38 },
+    { name: "Réka", age: 24 },
+    { name: "Ákos", age: 29 }
+];
+
+
